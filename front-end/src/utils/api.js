b321ceaf05c5c05791c89673be02c340f7ecbae4
Jacob <jacobgruen1@gmail.com>
2021-11-19 18:32:37 -0500

    backend-url

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,7 +5,7 @@
  import formatReservationDate from "./format-reservation-date";
  import formatReservationTime from "./format-reservation-date";
  
- const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8000";
+ const API_BASE_URL = 'https://jg-reservations-backend.herokuapp.com/' || "http://localhost:8000";
  
  /**
   * Defines the default headers for these functions to work with `json-server`
