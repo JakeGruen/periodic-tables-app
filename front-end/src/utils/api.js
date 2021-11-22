4f10424136f5464f3f0b0de6c001c6b7f31bdda4
Jacob <jacobgruen1@gmail.com>
2021-11-22 10:58:21 -0500

    fix-api

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,7 +5,7 @@
  import formatReservationDate from "./format-reservation-date";
  import formatReservationTime from "./format-reservation-date";
  
- const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://localhost:8000';
  
  /**
   * Defines the default headers for these functions to work with `json-server`
